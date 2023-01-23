145f9e28a933c531511518829a9c0037bcc44385
Alexandre Tihonovs <talexone@gmail.com>
2023-01-23 07:43:02 +0100

    add sejda api

--- zip2pdf.bat
+++ zip2pdf.bat
@@ -1,6 +1,11 @@
-#@echo off
 setlocal
 set destdir=%~d1%~p1%~n1
 cd /d %~dp0
 
 unzip -o %1 -d "%destdir%" 
+
+call sejda-console merge -f "%destdir%\*.pdf" -o "%destdir%".pdf --overwrite
+
+del "%destdir%\*" /q
+
+rmdir "%destdir%" /q
